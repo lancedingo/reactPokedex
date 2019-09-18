5a6d7e5f535fe1ab4adeba3dd38877e7cab93596
Rian <rian_laney@yahoo.com>
2019-09-18 10:49:55 -0400

    first commit

---
+++ src/components/PokeCell.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles/PokeCell.css';
+import sprites from '../assets/sprites.png';
+
+
+function PokeCell({ pokeClass, handleOnClick }) {
+  const { id, backgroundPosition } = pokeClass;
+  const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
+
+  return (
+    <button onClick={() => handleOnClick(id)} style={style} className="poke-cell">
+    
+    </button>
+  )
+  }
+
+
+
+
+export default PokeCell;
